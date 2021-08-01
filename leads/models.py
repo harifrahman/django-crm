420a44a5dc57cb2a1fb12d886c7f0a2f30f2ee52
flaster <harifrahman999@gmail.com>
2021-08-01 22:50:07 +0700

    lead models orm

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,32 @@
+from typing import SupportsRound
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Lead(models.Model):
+
+    # GENDER = (
+    #     ('male', 'male'),
+    #     ('female', 'female')
+    # )
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    
+    # is_active = models.BooleanField(default=True)
+    # source = models.CharField(choices=GENDER, max_length=100)
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # profile_file = models.FileField(blank=True, null=True)
+
+    agent = models.ForeignKey("Agent", on_delete=CASCADE)
+
+
+class Agent(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
