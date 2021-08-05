0ddcb0370b1c7d3c0781013d7df559139cda75a7
flaster <harifrahman999@gmail.com>
2021-08-05 11:43:54 +0700

    Hands on Simple QuerySets and Managers

--- leads/models.py
+++ leads/models.py
@@ -2,11 +2,15 @@ from typing import SupportsRound
 from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
-User = get_user_model()
+# User = get_user_model()
 
 # Create your models here.
+class User(AbstractUser):
+    pass 
+
 class Lead(models.Model):
 
     # GENDER = (
@@ -25,8 +29,12 @@ class Lead(models.Model):
 
     agent = models.ForeignKey("Agent", on_delete=CASCADE)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Agent(models.Model):
     user = models.ForeignKey(User, on_delete=CASCADE)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.email
