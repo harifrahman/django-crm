7ed82e1c7b584ff5cdd7b85b42652f1ba7f53056
flaster <harifrahman999@gmail.com>
2021-08-05 17:04:36 +0700

    passing data to views using Context

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Lead
 
 # Create your views here.
 
@@ -8,4 +9,11 @@ def homePage(request):
     # using manual template, set folder templates/leads
     # return render(request, "leads/home_page.html") 
     # but we can set views template too at setting , find DIR, and set the base dir of templates folder (views folder)
-    return render(request, "second_page.html")
+    leads = Lead.objects.all() 
+    context = {
+        "fullname": "John Doe",
+        "type": "internship",
+        "leads": leads
+    }
+
+    return render(request, "second_page.html", context)
