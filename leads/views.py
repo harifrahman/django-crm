2817d60bae61db96266c4d668a15d373f69a9bea
flaster <harifrahman999@gmail.com>
2021-08-05 15:02:14 +0700

    templates of view

--- leads/views.py
+++ leads/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def homePage(request):
-    return HttpResponse("Hello there ~")
+    # return HttpResponse("Hello there ~")
+    # using manual template, set folder templates/leads
+    # return render(request, "leads/home_page.html") 
+    # but we can set views template too at setting , find DIR, and set the base dir of templates folder (views folder)
+    return render(request, "second_page.html")
