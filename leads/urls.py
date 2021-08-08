70361577077b7d403eafc5a45e03656a7aab4330
flaster <harifrahman999@gmail.com>
2021-08-08 19:03:00 +0700

    url names

--- leads/urls.py
+++ leads/urls.py
@@ -4,9 +4,9 @@ from .views import leadList, leadDetail, leadCreate, leadUpdate, leadDelete
 app_name = "leads"
 
 urlpatterns = [
-    path('', leadList),
-    path('<int:pk>/', leadDetail),
-    path('create/', leadCreate),
-    path('<int:pk>/update', leadUpdate),
-    path('<int:pk>/delete', leadDelete),
+    path('', leadList, name='lead-list'),
+    path('<int:pk>/', leadDetail, name='lead-detail'),
+    path('create/', leadCreate, name='lead-create'),
+    path('<int:pk>/update', leadUpdate, name='lead-update'),
+    path('<int:pk>/delete', leadDelete, name='lead-delete'),
 ]
